d0b1fe365e65519ea64638d16a6839e2c83f5600
Evgeny Reznichenko <kusakyky@gmail.com>
2013-09-24 11:49:30 +0400

    Update angular to 1.2 Config bower

--- examples/angular/02_sortabl/public/js/controllers/article.table.js
+++ examples/angular/02_sortabl/public/js/controllers/article.table.js
@@ -10,11 +10,19 @@ angular
     ])
     .controller('ArticleTable', ['$scope', 'Articles', '$window', function ($scope, Articles, $window) {
         $scope.articles = [];
-        $scope.count = 50;
+        $scope.count = 10;
 
         //
         angular.element($window).on('scroll', function () {
-            
+            var $el = angular.element('#list'),
+                $w = angular.element($window),
+                wh = $w.height(),
+                eh = $el.height(),
+                st = $w.scrollTop();
+
+            if (0 > eh - wh - st) {
+                $scope.getMore();
+            }
         });
 
         $scope.getMore = function () {
